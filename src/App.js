6d889e3735bc39d62f56f27da376f208794a763a
Siddhant <siddhantahireofficial@gmail.com>
2020-11-16 16:11:21 +0530

    react router done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import { Navs } from './components/Navs';
+import { Home } from './pages/Home';
+import { Starred } from './pages/Starred';
+
 
 function App() {
   return (
-    <div>
-      hii
-    </div>
+    <Navs>
+      <Switch>
+        <Route exact path="/"><Home/></Route>
+        <Route exact path="/starred"><Starred/></Route>
+        <Route>
+          404 Page Not Found
+        </Route>
+      </Switch>
+    </Navs>
   );
 }
 
---
+++ src/pages/Starred.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Starred = () => {
+    return (
+        <div>
+            This is Starred Page.
+        </div>
+    )
+}
